8adb69c146683ade1be2960b72d977843907dfe4
Jean-Michel <jean-michel.picod@cassidian.com>
2011-02-07 13:10:49 +0100

    * Passage du SID en uppercase * Ajout de RPC_SID pour la Masterkey * Correction du parser de Masterkey pour les DomainKey

---
+++ DPAPI/credhist.py
@@ -0,0 +1,304 @@
+#!/usr/bin/env python
+
+#############################################################################
+##                                                                         ##
+## This file is part of DPAPIck                                            ##
+## Windows DPAPI decryption & forensic toolkit                             ##
+##                                                                         ##
+## Copyright (C) Jean-Michel Picod <jmichel.p@gmail.com>                   ##
+## Copyright (C) Elie Bursztein <elie@elie.im>                             ##
+##                                                                         ##
+## This program is free software; you can redistribute it and/or modify it ##
+## under the terms of the GNU General Public License version 2 as          ##
+## published by the Free Software Foundation.                              ##
+##                                                                         ##
+## This program is distributed in the hope that it will be useful, but     ##
+## WITHOUT ANY WARRANTY; without even the implied warranty of              ##
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU       ##
+## General Public License for more details.                                ##
+##                                                                         ##
+#############################################################################
+
+import struct
+import string
+from crypto import *
+import hashlib
+from M2Crypto import *
+
+class RPC_SID:
+    def __init__(self, raw):
+        self._rawLen = 0
+        fmt = "<8B"
+        tmp = struct.unpack_from(fmt, raw, self._rawLen)
+        self._rawLen += struct.calcsize(fmt)
+        self._version = tmp[0]
+        self._nbSubAuthority = tmp[1]
+        self._idAuthority = tmp[2:]
+        fmt = "<%dL" % (self._nbSubAuthority)
+        tmp = struct.unpack_from(fmt, raw, self._rawLen)
+        self._rawLen += struct.calcsize(fmt)
+        self._SubAuthorities = tmp
+
+    def __str__(self):
+        from array import array
+        a = array("B", [0, 0])
+        a.extend(self._idAuthority)
+        authority = struct.unpack("!Q", a.tostring())[0]
+        s = "S-%d-%d" % (self._version, authority)
+        for i in range(self._nbSubAuthority):
+            s += "-%d" % (self._SubAuthorities[i])
+        return s
+
+    def getRawLength(self):
+        return self._rawLen
+
+    def __repr__(self):
+        return """
+        RPC_SID(%s):
+            revision: %d
+            identifier-authority: %r
+            subAuthorities: %r
+            """ % (self, self._version, self._idAuthority, self._SubAuthorities)
+
+class CredhistEntry:
+    """Represents an entry in the Credhist file"""
+    
+    def __init__(self, raw):
+        """Initialize the object with raw bytes"""
+        self._raw = raw
+        self._clear = None
+        self.isParsed = False
+
+    def setPassword(self, p):
+        self._clear = p
+
+    def parse(self):
+        """Unpack the object and returns boolean wether it was successfull or not"""
+        try:
+            args = { "offset": 0, "fmt": "", "buffer": self._raw }
+            args["fmt"] = "<7L"
+            tmp = struct.unpack_from(args["fmt"], args["buffer"], args["offset"])
+            args["offset"] += struct.calcsize(args["fmt"])
+            self._magic = tmp[0]
+            self._algoHash = CryptoAlgo(tmp[1])
+            self._rounds = tmp[2]
+            self._todo = tmp[3]
+            self._algoCipher = CryptoAlgo(tmp[4])
+            self._dataLen = tmp[5]
+            self._hmacLen = tmp[6]
+
+            ## IV
+            args["fmt"] = "<16B"
+            tmp = struct.unpack_from(args["fmt"], args["buffer"], args["offset"])
+            args["offset"] += struct.calcsize(args["fmt"])
+            self._iv = "".join(map(chr,tmp))
+
+            ## RPC_SID
+            self._userSID = RPC_SID(args["buffer"][args["offset"]:])
+            args["offset"] += self._userSID.getRawLength()
+
+            n = self._dataLen + self._hmacLen
+            if n % self._algoCipher.blockSize() > 0:
+                n = self._algoCipher.blockSize() * (n / self._algoCipher.blockSize() + 1)
+            args["fmt"] = "<%uB" % (n)
+            tmp = struct.unpack_from(args["fmt"], args["buffer"], args["offset"])
+            args["offset"] += struct.calcsize(args["fmt"])
+            self._encrypted = "".join(map(chr,tmp))
+
+            args["fmt"] = "<L16BL"
+            tmp = struct.unpack_from(args["fmt"], args["buffer"], args["offset"])
+            args["offset"] += struct.calcsize(args["fmt"])
+            self._magic2 = tmp[0]
+            self._guid = "".join(map(chr,tmp[1:17]))
+            self._size = tmp[-1]
+
+        except Exception as e:
+            self.isParsed = False
+        else:
+            self.isParsed = True
+            self.isEncrypted = True
+        finally:
+            return self.isParsed
+
+    def __repr__(self):
+        d = {
+            "guid": self._guid.encode('hex'),
+            "magic": self._magic,
+            "algo": self._algoHash,
+            "rounds": self._rounds,
+            "todo": self._todo,
+            "cipher": self._algoCipher,
+            "dataLen": self._dataLen,
+            "hmacLen": self._hmacLen,
+            "iv": self.getIV().encode('hex'),
+            "userSID": self._userSID,
+            "encrypted": self._encrypted.encode('hex'),
+            "magic2": self._magic2,
+            "entrysize": self._size,
+            "data": "<ENCRYPTED>",
+            "hmac": "<ENCRYPTED>",
+            "password": ""
+        }
+        if self._clear != None:
+            d["password"] = "'%s'" % (self._clear)
+        if self.isEncrypted == False:
+            d["data"] = self._password.encode('hex')
+            d["hmac"] = self._hmac.encode('hex')
+        if self.isParsed == True:
+            return """
+    CredHistEntry(%(guid)s): %(password)s
+        dwMagic: %(magic)#x
+        dwAlgoHash: %(algo)r
+        dwRounds: %(rounds)#x
+        dwTodo: %(todo)#x
+        dwCipherAlgo: %(cipher)r
+        dwDataLen: %(dataLen)#x
+        dwHmacLen: %(hmacLen)#x
+        bIV: %(iv)s
+        UserSID: %(userSID)r
+        bEncrypted: %(encrypted)s
+        bData: %(data)s
+        bHmac: %(hmac)s
+        dwMagic2: %(magic2)#x
+        bGUID: %(guid)s
+        dwEntrySize: %(entrysize)u (%(entrysize)#x)
+    """ % d
+        else:
+            return "CredHistEntry(RAW): %s" % (self._raw.encode('hex'))
+
+    def getIV(self):
+        if self.isParsed == True:
+            return self._iv
+        else:
+            return None
+
+    def getGUID(self):
+        if self.isParsed == True:
+            return self._guid
+        else:
+            return None
+
+    def getUserSID(self):
+        if self.isParsed == True:
+            return str(self._userSID)
+        else:
+            return None
+
+    def decryptWithHash(self, h):
+        if self.isParsed == False:
+            return False
+        sid = self.getUserSID()
+        sid += "\0"
+        sid = sid.encode("UTF-16LE")
+        cleartxt = dataDecrypt(self._encrypted,
+                h, sid, self._algoCipher,
+                self.getIV(), self._algoHash, self._rounds)
+        self._password = cleartxt[:self._dataLen]
+        self._hmac = cleartxt[self._dataLen:]
+        self._hmac = self._hmac[:16]
+        ##TODO: Compute & Verify HMAC
+        self.isEncrypted = False
+        return True
+
+    def decryptWithPassword(self, password):
+        m = hashlib.sha1()
+        m.update(password.encode("UTF-16LE"))
+        return self.decryptWithHash(m.digest())
+
+    def getHash(self):
+        if self.isEncrypted == True:
+            return None
+        else:
+            return self._password
+
+class CredHistPool:
+    def __init__(self, raw):
+        self._dico = dict()
+        self._guid = dict()
+        self._pool = []
+        self._raw = raw
+
+    def parse(self):
+        total = len(self._raw)
+        size = struct.unpack("<L", self._raw[-4:])
+        while size[0] != 0:
+            if size[0] > total:
+                return False
+            total -= size[0]
+            self.addEntry(self._raw[total:])
+            size = struct.unpack_from("<L", self._raw[total - struct.calcsize("<L"):])
+        if total > 0:
+            footer = struct.unpack_from("<L16BL", self._raw)
+            self._currpw = ''.join(map(chr, footer[1:-1]))
+            self._footMagic = footer[0]
+        return True
+
+    def addEntry(self, blob):
+        x = CredhistEntry(blob)
+        try:
+            x.parse()
+        except Exception:
+            return False
+        else:
+            self._guid[x.getGUID()] = x
+            self._guid[x.getGUID().encode('hex')] = x
+            self._pool.append(x)
+            return True
+
+    def lookupGUID(self, guid):
+        if guid == self._currpw:
+            return self._curhash
+        if guid in self._guid:
+            return self._guid[guid].getHash()
+        else:
+            return None
+
+    def lookupHash(self, h):
+        if h in self._dico:
+            return self._dico[h]
+        else:
+            return None
+
+    def addPassword(self, pwd):
+        m = hashlib.sha1()
+        m.update(pwd.encode("UTF-16LE"))
+        self._dico[m.digest()] = pwd
+        self._dico[m.hexdigest()] = pwd
+        self._curhash = m.digest()
+
+    def addWordlist(self, filename):
+        f = open(filename, "r")
+        for w in f:
+            self.addPassword(w.rstrip())
+ 
+    def decryptWithHash(self, h):
+        curhash = h
+        for entry in self._pool:
+            try:
+                entry.decryptWithHash(curhash)
+                curhash = entry.getHash()
+                p = self.lookupHash(curhash)
+                if p != None:
+                    entry.setPassword(p)
+            except Exception:
+                return False
+        return True
+
+    def decryptWithPassword(self, pwd):
+        m = hashlib.sha1()
+        m.update(pwd.encode("UTF-16LE"))
+        self._dico[m.digest()] = pwd
+        self._dico[m.hexdigest()] = pwd
+        return self.decryptWithHash(m.digest())
+
+    def __repr__(self):
+        return """
+CredHistPool:
+%s,
+pool = %r,
+dico = %r
+""" % (self._currpw.encode('hex'), self._pool, self._dico)
+    
+
+
+# vim:ts=4:expandtab:sw=4
---
+++ DPAPI/crypto.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env python
+
+#############################################################################
+##                                                                         ##
+## This file is part of DPAPIck                                            ##
+## Windows DPAPI decryption & forensic toolkit                             ##
+##                                                                         ##
+## Copyright (C) Jean-Michel Picod <jmichel.p@gmail.com>                   ##
+## Copyright (C) Elie Bursztein <elie@elie.im>                             ##
+##                                                                         ##
+## This program is free software; you can redistribute it and/or modify it ##
+## under the terms of the GNU General Public License version 2 as          ##
+## published by the Free Software Foundation.                              ##
+##                                                                         ##
+## This program is distributed in the hope that it will be useful, but     ##
+## WITHOUT ANY WARRANTY; without even the implied warranty of              ##
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU       ##
+## General Public License for more details.                                ##
+##                                                                         ##
+#############################################################################
+
+import hashlib
+import hmac
+import struct
+from M2Crypto import *
+
+def CryptDeriveKey(h, digest='sha1'):
+    _dg = getattr(hashlib, digest)
+    if len(h) > 64:
+        h = _dg(h).digest()
+    ipad = "\x36" * len(h)
+    opad = "\x5C" * len(h)
+    ipad = "".join([chr(ord(x) ^ ord(y)) for (x, y) in zip(h, ipad)])
+    opad = "".join([chr(ord(x) ^ ord(y)) for (x, y) in zip(h, opad)])
+    if len(ipad) < 64:
+        ipad = ipad + "\x36" * (64 - len(ipad))
+    if len(opad) < 64:
+        opad = opad + "\x5C" * (64 - len(opad))
+    tmp = _dg(ipad).digest() + _dg(opad).digest()
+    tmp = [ord(x) for x in tmp]
+    for i in range(0, len(tmp)):
+        if sum([int(x) for x in bin(tmp[i])[2:]]) % 2 == 0:
+            if tmp[i] % 2 == 1:
+                tmp[i] -= 1
+            else:
+                tmp[i] += 1
+    return "".join(map(chr, tmp))
+    
+def pbkdf2(passphrase, salt, keylen, iterations, digest='sha1', mac=hmac):
+    _dg = getattr(hashlib, digest)
+    buff = ""
+    i = 0
+    while len(buff) < keylen:
+        i += 1
+        init = mac.new(passphrase, salt + struct.pack("!L", i), _dg).digest()
+        U = init
+        for j in range(1, iterations):
+            U = mac.new(passphrase, init, _dg).digest()
+            init = "".join([chr(ord(x) ^ ord(y)) for (x, y) in zip(init, U)])
+        buff += init
+    return buff[:keylen]
+
+_dict = {
+    0x6603: { "name": "DES3", "keyLength": 192, "IVLength": 64, "blockLength": 64, "m2": "des_ede3_cbc" },
+    0x6609: { "name": "DES2", "keyLength": 128, "IVLength": 64, "blockLength": 64, "m2": "des_ede_cbc" },
+    0x6611: { "name": "AES", "keyLength": 128, "IVLength": 128, "blockLength": 128, "m2": "aes_128_cbc" },
+    0x660e: { "name": "AES-128", "keyLength": 128, "IVLength": 128, "blockLength": 128, "m2": "aes_128_cbc" },
+    0x660f: { "name": "AES-192", "keyLength": 192, "IVLength": 192, "blockLength": 128, "m2": "aes_192_cbc" },
+    0x6610: { "name": "AES-256", "keyLength": 256, "IVLength": 256, "blockLength": 128, "m2": "aes_256_cbc" },
+    0x6601: { "name": "DES", "keyLength": 64, "IVLength": 64, "blockLength": 64, "m2": "des_cbc" },
+
+    0x8009: { "name": "HMAC", "digestLength": 160, "blockLength": 512 },
+    0x8005: { "name": "MAC", "keyLength": 0, "IVLength": 0, "blockLength": 0 },
+    0x8001: { "name": "md2", "keyLength": 0, "IVLength": 0, "blockLength": 0 },
+    0x8002: { "name": "md4", "keyLength": 0, "IVLength": 0, "blockLength": 0 },
+    0x8003: { "name": "md5", "keyLength": 0, "IVLength": 0, "blockLength": 0 },
+
+    0x6602: { "name": "RC2", "keyLength": 0, "IVLength": 0, "blockLength": 0 },
+    0x6801: { "name": "RC4", "keyLength": 0, "IVLength": 0, "blockLength": 0 },
+    0x660d: { "name": "RC5", "keyLength": 0, "IVLength": 0, "blockLength": 0 },
+
+    0x8004: { "name": "sha1", "digestLength": 160, "blockLength": 512 },
+    0x800c: { "name": "sha256", "digestLength": 256, "blockLength": 512 },
+    0x800d: { "name": "sha384", "digestLength": 384, "blockLength": 1024 },
+    0x800e: { "name": "sha512", "digestLength": 512, "blockLength": 1024 },
+    }
+
+class CryptoAlgo:
+    def __init__(self, i):
+        self._algo = i
+        self._dict = _dict
+
+    def value(self):
+        return self._algo
+
+    def m2name(self):
+        return self._dict[self._algo]["m2"]
+
+    def keyLength(self):
+        return self._dict[self._algo]["keyLength"] / 8
+
+    def ivLength(self):
+        return self._dict[self._algo]["IVLength"] / 8
+
+    def blockSize(self):
+        return self._dict[self._algo]["blockLength"] / 8
+
+    def digestLength(self):
+        return self._dict[self._algo]["digestLength"] / 8
+
+    def str(self):
+        return "%s" % self
+    
+    def __str__(self):
+        return self._dict[self._algo]["name"]
+
+    def __repr__(self):
+        return "%s [%#x]" % (self._dict[self._algo]["name"], self._algo)
+
+def dataDecrypt(raw, password, hmacSalt, cipher, cipherSalt, h, rounds):
+    hh = h.str()
+    if hh == "HMAC":
+        hh = "sha1"
+    dg = getattr(hashlib, hh)
+    encKey = hmac.new(password, hmacSalt, dg).digest()
+    tmp = pbkdf2(encKey, cipherSalt, cipher.keyLength() + cipher.ivLength(),
+            rounds, hh)
+    cipher = EVP.Cipher(cipher.m2name(),
+            tmp[:cipher.keyLength()],
+            tmp[cipher.keyLength():],
+            m2.decrypt, 0)
+    cipher.set_padding(0)
+    cleartxt = cipher.update(raw)
+    cipher.final()
+    return cleartxt
+
+def DpapiHmac(password, hmacSalt, hash, salt2, value):
+    hh = hash.str()
+    if hh == "HMAC":
+        hh = "sha1"
+    dg = getattr(hashlib, hh)
+    encKey = hmac.new(password, hmacSalt, dg).digest()
+    tmpmac = hmac.new(encKey, salt2, dg).digest()
+    return hmac.new(tmpmac, value, dg).digest()
+
+# vim:ts=4:expandtab:sw=4
