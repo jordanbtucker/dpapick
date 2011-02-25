cf4b681398a8154ab8a0d3aef0f5959123323b45
Phil <phil@secdev.org>
2011-02-25 19:59:51 +0100

    Reworked DPAPI blob * * * * * * Added main program

--- DPAPI/blob.py
+++ DPAPI/blob.py
@@ -19,207 +19,95 @@
 ##                                                                         ##
 #############################################################################
 
-import struct
-import string
 from crypto import *
 import hashlib
 from M2Crypto import *
+from eater import Eater, DataStruct
 
-class DpapiBlob:
-    def __init__(self, raw):
-        self._raw = raw
-        self.isParsed = False
-        self.isEncrypted = True
+class DPAPIBlob(DataStruct):
+    def __init__(self, raw=None):
+        self.clearText = None
+        DataStruct.__init__(self, raw)
 
-    def decrypt(self, masterkey, entropy = "", strongPassword = ""):
-        if self.isParsed == False:
-            return False
-        if self.isEncrypted == False:
-            return True
+    def parse(self, data):
+        self.providers = []
+        nb = data.eat("L")
+        while nb > 0:
+            p = data.eat("L2H8B")
+            nb -= 1
+            self.providers.append("%0x-%0x-%0x-%0x%0x-%0x%0x%0x%0x%0x%0x" % p)
 
+        self.guid = []
+        nb = data.eat("L")
+        while nb > 0:
+            p = data.eat("L2H8B")
+            nb -= 1
+            self.guid.append("%0x-%0x-%0x-%0x%0x-%0x%0x%0x%0x%0x%0x" % p)
+
+        self.flags = data.eat("L")
+        self.description = data.eat_length_and_string("L").decode("UTF-16LE")
+        self.cipherAlgo = CryptoAlgo( data.eat("L") )
+        self.keyLen = data.eat("L")
+        self.data = data.eat_length_and_string("L")
+        data.eat("L")
+        self.hashAlgo = CryptoAlgo(data.eat("L"))
+        self.hashLen = data.eat("L")
+        self.salt = data.eat_length_and_string("L")
+        self.cipherText = data.eat_length_and_string("L")
+        self.crc = data.eat_length_and_string("L")
+
+
+    def decrypt(self, masterkey, entropy=None, strongPassword=None):
         ## First compute the weird HMAC...
-        digest = hashlib.new(self._hashAlgo.str())
         if len(masterkey) > 63:
-            digest.update(masterkey)
-            masterkey = digest.digest()
-            digest = hashlib.new(self._hashAlgo.str())
-        ipad = "\x36" * len(masterkey)
-        opad = "\x5C" * len(masterkey)
-        ipad = "".join([chr(ord(x) ^ ord(y)) for (x, y) in zip(masterkey, ipad)])
-        opad = "".join([chr(ord(x) ^ ord(y)) for (x, y) in zip(masterkey, opad)])
-        if len(ipad) < 64:
-            ipad = ipad + "\x36" * (64 - len(ipad))
-        if len(opad) < 64:
-            opad = opad + "\x5C" * (64 - len(opad))
+            dg = hashlib.new(self.hashAlgo.name)
+            dg.update(masterkey)
+            masterkey = dg.digest()
+
+        masterkey += "\0"*64
+        ipad = "".join(chr(ord(masterkey[i])^0x36) for i in range(64))
+        opad = "".join(chr(ord(masterkey[i])^0x5c) for i in range(64))
+
+        digest = hashlib.new(self.hashAlgo.name)
         digest.update(ipad)
-        digest.update(self._data)
+        digest.update(self.data)
         tmp = digest.digest()
-        digest = hashlib.new(self._hashAlgo.str())
+
+        digest = hashlib.new(self.hashAlgo.name)
         digest.update(opad)
         digest.update(tmp)
-        if entropy != None and len(entropy) > 0:
+        if entropy is not None:
             digest.update(entropy)
-        if strongPassword != None and len(strongPassword) > 0:
+        if strongPassword is not None:
             digest.update(strongPassword)
+        final = digest.digest()
 
         ## Derive keys from it
-        keys = CryptDeriveKey(digest.digest(), self._hashAlgo.str())
+        keys = CryptDeriveKey(final, self.hashAlgo.name)
 
         ## Decrypt
-        cipher = EVP.Cipher(self._cipherAlgo.m2name(), keys[:self._cipherAlgo.keyLength()], "\x00" * self._cipherAlgo.ivLength(), m2.decrypt, 0)
+        cipher = EVP.Cipher(self.cipherAlgo.m2name, keys[:self._cipherAlgo.keyLength()], 
+                            "\x00"*self.cipherAlgo.ivLength, m2.decrypt, 0)
         cipher.set_padding(0)
-        cleartext = cipher.update(self._cipherText)
-        cipher.final()
+        self.clearText = cipher.update(self.cipherText) + cipher.final()
 
         ##TODO: check against provided HMAC
-        self._clearText = cleartext
-        self.isEncrypted = False
-        return True
-
-    def getMasterkeyGUID(self):
-        return self._keysGUID[0]
-
-    def getCleartext(self):
-        if self.isEncrypted == True:
-            return None
-        return self._clearText
-
-    def parse(self):
-        try:
-            args = { "offset": 0, "fmt": "", "buffer": self._raw }
-
-            ## Crypto providers
-            args["fmt"] = "<L"
-            tmp = struct.unpack_from(args["fmt"], args["buffer"], args["offset"])
-            args["offset"] += struct.calcsize(args["fmt"])
-            self._providers = []
-            for i in range(0, tmp[0]):
-                args["fmt"] = "<L2H8B"
-                tmp = struct.unpack_from(args["fmt"], args["buffer"], args["offset"])
-                args["offset"] += struct.calcsize(args["fmt"])
-                self._providers.append("%0x-%0x-%0x-%0x%0x-%0x%0x%0x%0x%0x%0x" % tmp)
-
-            ## Keys GUID
-            args["fmt"] = "<L"
-            tmp = struct.unpack_from(args["fmt"], args["buffer"], args["offset"])
-            args["offset"] += struct.calcsize(args["fmt"])
-            self._keysGUID = []
-            for i in range(0, tmp[0]):
-                args["fmt"] = "<L2H8B"
-                tmp = struct.unpack_from(args["fmt"], args["buffer"], args["offset"])
-                args["offset"] += struct.calcsize(args["fmt"])
-                self._keysGUID.append("%0x-%0x-%0x-%0x%0x-%0x%0x%0x%0x%0x%0x" % tmp)
-
-            ## Description
-            args["fmt"] = "<2L"
-            tmp = struct.unpack_from(args["fmt"], args["buffer"], args["offset"])
-            args["offset"] += struct.calcsize(args["fmt"])
-            self._flags = tmp[0]
-            self._descrLen = tmp[1]
-
-            args["fmt"] = "<%dH" % (tmp[1] / 2)
-            tmp = struct.unpack_from(args["fmt"], args["buffer"], args["offset"])
-            args["offset"] += struct.calcsize(args["fmt"])
-            self._description = "".join(map(chr, tmp))
-
-            args["fmt"] = "<3L"
-            tmp = struct.unpack_from(args["fmt"], args["buffer"], args["offset"])
-            args["offset"] += struct.calcsize(args["fmt"])
-            self._cipherAlgo = CryptoAlgo(tmp[0])
-            self._keyLen = tmp[1]
-            self._dataLen = tmp[2]
-
-            args["fmt"] = "<%dB" % (self._dataLen)
-            tmp = struct.unpack_from(args["fmt"], args["buffer"], args["offset"])
-            args["offset"] += struct.calcsize(args["fmt"])
-            self._data = "".join(map(chr, tmp))
-
-            args["fmt"] = "<4L"
-            tmp = struct.unpack_from(args["fmt"], args["buffer"], args["offset"])
-            args["offset"] += struct.calcsize(args["fmt"])
-            self._hashAlgo = CryptoAlgo(tmp[1])
-            self._hashLen = tmp[2]
-            self._saltLen = tmp[3]
-
-            args["fmt"] = "<%dB" % (self._saltLen)
-            tmp = struct.unpack_from(args["fmt"], args["buffer"], args["offset"])
-            args["offset"] += struct.calcsize(args["fmt"])
-            self._salt = "".join(map(chr, tmp))
-
-            args["fmt"] = "<L"
-            tmp = struct.unpack_from(args["fmt"], args["buffer"], args["offset"])
-            args["offset"] += struct.calcsize(args["fmt"])
-            self._cipherLen = tmp[0]
-
-            args["fmt"] = "<%dB" % (self._cipherLen)
-            tmp = struct.unpack_from(args["fmt"], args["buffer"], args["offset"])
-            args["offset"] += struct.calcsize(args["fmt"])
-            self._cipherText = "".join(map(chr, tmp))
-
-            args["fmt"] = "<L"
-            tmp = struct.unpack_from(args["fmt"], args["buffer"], args["offset"])
-            args["offset"] += struct.calcsize(args["fmt"])
-            self._crcLen = tmp[0]
-
-            args["fmt"] = "<%dB" % (self._crcLen)
-            tmp = struct.unpack_from(args["fmt"], args["buffer"], args["offset"])
-            args["offset"] += struct.calcsize(args["fmt"])
-            self._crc = "".join(map(chr, tmp))
-
-        except Exception as e:
-            self.isParsed = False
-        else:
-            self.isParsed = True
-            self.isEncrypted = True
-        finally:
-            self._clearText = None
-            return self.isParsed
+        self.decrypted = True
 
     def __repr__(self):
-        if self.isParsed == False:
-            return """
-            DPAPI Blob (RAW): %s""" % self._raw.encode('hex')
-        else:
-            args = {
-                    "providers": self._providers,
-                    "mkey": self._keysGUID,
-                    "flags": self._flags,
-                    "descr": self._description,
-                    "cipherAlgo": self._cipherAlgo,
-                    "keyLen": self._keyLen,
-                    "dataLen": self._dataLen,
-                    "data": self._data.encode('hex'),
-                    "hashAlgo": self._hashAlgo,
-                    "hashLen": self._hashLen,
-                    "saltLen": self._saltLen,
-                    "salt": self._salt.encode('hex'),
-                    "cipherLen": self._cipherLen,
-                    "cipher": self._cipherText.encode('hex'),
-                    "crcLen": self._crcLen,
-                    "crc": self._crc.encode('hex'),
-                    "clearText": "<ENCRYPTED>"
-                    }
-            if self.isEncrypted == False:
-                args["clearText"] = self._clearText.encode('hex')
-            return """
-            DPAPI Blob:
-                Providers: %(providers)r
-                Masterkey GUID: %(mkey)r
-                Flags: %(flags)d (%(flags)#x)
-                Description: "%(descr)s"
-                CipherAlgo: %(cipherAlgo)r
-                KeyLen: %(keyLen)d (%(keyLen)#x)
-                DataLen: %(dataLen)d (%(dataLen)#x)
-                Data: %(data)s
-                HashAlgo: %(hashAlgo)r
-                HashLen: %(hashLen)d (%(hashLen)#x)
-                SaltLen: %(saltLen)d (%(saltLen)#x)
-                Salt: %(salt)s
-                CipherLen: %(cipherLen)d (%(cipherLen)#x)
-                Cipher: %(cipher)s
-                CrcLen: %(crcLen)d (%(crcLen)#x)
-                Crc: %(crc)s
-                ClearText: %(clearText)s
-                """ % args
+        s = ["DPAPI BLOB %r" % self.guid]
+        s.append("""        providers: %(providers)r
+        mkey: %(keysGUID)r
+        flags: %(self.flags)#x
+        descr: %(description)s
+        cipherAlgo: %(self.cipherAlgo)r
+        hashAlgo: %(hashAlgo)r""" % self.__dict__)
+        s.append("\tdata: %s" % self.data.encode('hex'))
+        s.append("\tsalt: %s" % self._salt.encode('hex'))
+        s.append("\tcipher: %s" % self._cipherText.encode('hex'))
+        s.append("\tcrc: %s" % self._crc.encode('hex'))
+        if self.clearText is not None:
+            s.append("\tcleartext: %s" % self.clearText)
+        return "\n".join(s)
 
 # vim:ts=4:expandtab:sw=4
