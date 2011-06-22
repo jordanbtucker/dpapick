0645777ce00acd9235202e599b47c224e907d55c
Jean-Michel <jean-michel.picod@cassidian.com>
2011-06-22 23:21:59 +0200

    Add CRC verification + correct padding setting

--- DPAPI/blob.py
+++ DPAPI/blob.py
@@ -33,6 +33,9 @@ class DPAPIBlob(DataStruct):
         self.version = data.eat("L")
         self.provider = "%0x-%0x-%0x-%0x%0x-%0x%0x%0x%0x%0x%0x" % data.eat("L2H8B")
 
+        ## For HMAC computation
+        blobStart = data.ofs
+
         self.guids = []
         nb = data.eat("L")
         while nb > 0:
@@ -45,11 +48,15 @@ class DPAPIBlob(DataStruct):
         self.cipherAlgo = CryptoAlgo( data.eat("L") )
         self.keyLen = data.eat("L")
         self.data = data.eat_length_and_string("L")
-        data.eat("L")
+        self.strong = data.eat_length_and_string("L")
         self.hashAlgo = CryptoAlgo(data.eat("L"))
         self.hashLen = data.eat("L")
         self.salt = data.eat_length_and_string("L")
         self.cipherText = data.eat_length_and_string("L")
+
+        ## For HMAC computation
+        self.blob = data.raw[blobStart:data.ofs]
+
         self.crc = data.eat_length_and_string("L")
 
 
@@ -59,10 +66,14 @@ class DPAPIBlob(DataStruct):
         keys = CryptDeriveKey(sessionkey, self.cipherAlgo, self.hashAlgo.name)
         cipher = EVP.Cipher(self.cipherAlgo.m2name, keys[:self.cipherAlgo.keyLength], 
                             "\x00"*self.cipherAlgo.ivLength, m2.decrypt, 0)
-        cipher.set_padding(0)
+        cipher.set_padding(1)
         self.clearText = cipher.update(self.cipherText) + cipher.final()
-        ##TODO: check against provided HMAC
-        
+
+        ## check against provided HMAC
+        self.crcComputed = CryptSessionKey(masterkey, self.salt, self.hashAlgo.name,
+                                           entropy=entropy,
+                                           strongPassword=self.blob)
+        self.decrypted = self.crcComputed == self.crc
 
     def __repr__(self):
         s = ["DPAPI BLOB"]
@@ -77,6 +88,8 @@ class DPAPIBlob(DataStruct):
         s.append("\tsalt        = %s" % self.salt.encode('hex'))
         s.append("\tcipher      = %s" % self.cipherText.encode('hex'))
         s.append("\tcrc         = %s" % self.crc.encode('hex'))
+        if self.crcComputed is not None:
+            s.append("\tcrcComputed = %s" % self.crcComputed.encode('hex'))
         if self.clearText is not None:
             s.append("\tcleartext   = %r" % self.clearText)
         return "\n".join(s)
