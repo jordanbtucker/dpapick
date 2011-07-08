ee9487ebf70b994f470e1f977fb9726e0a24bb6f
Jean-Michel <jean-michel.picod@cassidian.com>
2011-07-08 03:22:44 +0200

    Parser fixes + probe API

--- DPAPI/Probes/wifi.py
+++ DPAPI/Probes/wifi.py
@@ -34,6 +34,40 @@ class WirelessInfo(DataStruct):
             self.ssid = None
             DataStruct.__init__(self, raw)
 
+        _networktype = [ "Freq hopping", "Direct Sequence",
+                         "802.11a", "802.11b/g" ]
+        _networkinfra = [ "Ad-hoc", "Infrastructure", "Automatic" ]
+        _authmode = [ "Open", "Shared", "AutoSwitch", "WPA", "WPA-PSK",
+                      "WPA None", "WPA2", "WPA2-PSK" ]
+        _wifichans = {
+                2412000:  1,
+                2417000:  2,
+                2422000:  3,
+                2427000:  4,
+                2432000:  5,
+                2437000:  6,
+                2442000:  7,
+                2447000:  8,
+                2452000:  9,
+                2457000: 10,
+                2462000: 11,
+                2467000: 12,
+                2472000: 13,
+                2484000: 14,
+                5180000: 26,
+                5200000: 40,
+                5220000: 44,
+                5240000: 48,
+                5260000: 52,
+                5280000: 56,
+                5300000: 60,
+                5320000: 64,
+                5745000: 149,
+                5765000: 153,
+                5785000: 157,
+                5805000: 161
+                }
+
         def __repr__(self):
             s = ["WifiStruct"]
             flags = []
@@ -51,13 +85,13 @@ class WirelessInfo(DataStruct):
             s.append("        ssid             = %s" % self.ssid)
             s.append("        privacy          = 0x%x" % self.privacy) ##FIXME add prettyprint
             s.append("        rssi             = %i" % self.rssi)
-            s.append("        network type     = 0x%x" % self.nettype) ## FIXME repr
-            s.append("        configuration    = %s" % repr(self.configuration)) ##FIXME
-            s.append("        infrastructure   = %u" % self.infrastructuremode) ##FIXME
+            s.append("        network type     = %s" % self._networktype[self.nettype])
+            s.append("        channel          = %s" % self._wifichans[self.configuration[3]])
+            s.append("        infrastructure   = %s" % self._networkinfra[self.infrastructuremode])
             s.append("        rates            = %s" % repr(self.supportedrates)) ##FIXME repr
             s.append("        key index        = %u" % self.keyindex)
             s.append("        key              = %s" % self.key.encode('hex'))
-            s.append("        authentication   = 0x%x" % self.authmode) ##FIXME repr
+            s.append("        authentication   = %s" % self._authmode[self.authmode]) ##FIXME repr
             s.append("        802.1x           = %r" % (self.ieee8021xEnabled != 0))
             s.append("        eap flags        = 0x%x" % self.eapflags) ## FIXME repr
             eap = "0x%s" % self.eaptype
@@ -66,7 +100,7 @@ class WirelessInfo(DataStruct):
             if self.eaptype == 0x26:
                 eap = "PEAP"
             s.append("        eap type         = %s" % eap)
-            s.append("        auth data        = %s" % self.authdata)
+            #s.append("        auth data        = %s" % self.authdata)
             s.append("        wpa mcast cipher = 0x%x" % self.wpamcastcipher) ## FIXME repr
             s.append("        media type       = 0x%x" % self.mediatype) ## FIXME repr
             tmp = datetime.datetime.utcfromtimestamp(self.timestamp).ctime()
@@ -83,7 +117,7 @@ class WirelessInfo(DataStruct):
             self.privacy = data.eat("L") ## See NDIS_802_11_WEP_STATUS for values
             self.rssi = data.eat("l")
             self.nettype = data.eat("L")
-            self.configuration = data.eat("4L")
+            self.configuration = data.eat("8L")
             self.infrastructuremode = data.eat("L")
             self.supportedrates = data.eat("8B")
             self.keyindex = data.eat("L")
@@ -95,19 +129,21 @@ class WirelessInfo(DataStruct):
             self.ieee8021xEnabled = data.eat("L") != 0
             self.eapflags = data.eat("L")
             self.eaptype = data.eat("L") ## 0x13 = EAP-TLS / 0x26 = PEAP
-            self.authdata = data.eat_length_and_string("L")
+            self.authdatalen = data.eat("L")
+            self.authdata = data.eat("L")
             data.eat("2L") ## rdNetworkData - not used
             self.wpamcastcipher = data.eat("L") ## See NDIS_802_11_WEP_STATUS for values
             self.mediatype = data.eat("L") ## Should always be set to NdisMedium802_3
             data.eat("500B") ## pad
-            self.timestamp = (data.eat("Q"))
+            self.timestamp = data.eat("Q")
             if self.timestamp > 0: ## convert from FILETIME to EPOCH
-                self.timestamp -= 116444736000000000
                 self.timestamp /= 10000000
+                self.timestamp -= 11644473600
 
     def __init__(self, raw=None):
         self.wifiStruct = None
         self.dpapiblob = None
+        self.entropy = None
         DataStruct.__init__(self, raw)
         
     def parse(self, data):
@@ -115,10 +151,20 @@ class WirelessInfo(DataStruct):
         self.wifiStruct = WirelessInfo.WifiStruct(data.eat("%us" % l))
         self.dpapiblob = blob.DPAPIBlob(data.remain())
 
+    def preprocess(self, **k):
+        pass
+
+    def postprocess(self, **k):
+        xorKey = ("56660942080398014d67086611" * 5).decode('hex')
+        if self.decrypted:
+            self.cleartext = "".join([ chr(ord(x) ^ ord(y)) for (x,y) in zip(self.dpapiblob.cleartext, xorKey) ])
+
     def __repr__(self):
         s = ["Wirelesskey block"]
         s.append("        BSSID      = %s" % self.bssid)
         s.append("        SSID       = %s" % self.ssid)
+        if self.dpapiblob.decrypted:
+            s.append("        hexKey     = %s" % self.cleartext.encode('hex'))
         s.append("    %r" % self.wifiStruct)
         s.append("    %r" % self.dpapiblob)
         return "\n".join(s)
