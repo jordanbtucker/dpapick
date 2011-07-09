6c594d3a3a72b4b32d6f08a5e4a36c8bd6136a8c
Jean-Michel <jean-michel.picod@cassidian.com>
2011-07-09 10:17:58 +0200

    forget to add __init__ file

---
+++ DPAPI/__init__.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+#############################################################################
+##                                                                         ##
+## This file is part of DPAPIck                                            ##
+## Windows DPAPI decryption & forensic toolkit                             ##
+##                                                                         ##
+##                                                                         ##
+## Copyright (C) 2010, 2011 Cassidian SAS. All rights reserved.            ##
+## This document is the property of Cassidian SAS, it may not be copied or ##
+## circulated without prior licence                                        ##
+##                                                                         ##
+##  Author: Jean-Michel Picod <jean-michel.picod@cassidian.com>            ##
+##                                                                         ##
+## This program is distributed under dual cumulative licences:             ##
+##    * GPLv3 for non-commercial use of this program (see LICENCE.GPLv3)   ##
+##    * EADS licence for commercial use (see LICENCE.EADS)                 ##
+##                                                                         ##
+## If you want to make a commercial tool using this program, contact the   ##
+## author for information and a quotation                                  ##
+##                                                                         ##
+#############################################################################
+
+__all__ = [ 'Core', 'probe', 'Probes' ]
+
+# vim:ts=4:expandtab:sw=4
