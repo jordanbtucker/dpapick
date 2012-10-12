7fd09b090cfb3d9257a41f2774a07a7a315191c6
Jean-Michel <jean-michel.picod@cassidian.com>
2012-10-12 15:25:18 +0200

    Update python-registry to the latest version

--- depends/python-registry/samples/timeline.py
+++ depends/python-registry/samples/timeline.py
@@ -1,16 +1,21 @@
+#!/usr/bin/python
+
 import sys
 from Registry import *
 
 items = []
 
 def rec(key):
-    items.append((key.timestamp(), key.path()))
+    try:
+        items.append((key.timestamp(), key.path()))
+    except ValueError:
+        pass
     for subkey in key.subkeys():
         rec(subkey)
 
-
-reg = Registry.Registry(sys.argv[1])
-rec(reg.root())
+for path in sys.argv[1:]: 
+    reg = Registry.Registry(path)
+    rec(reg.root())
 
 for i in sorted(items, key=lambda x: x[0]):
     print "%s\t%s" % i
