240bbb55512cc79ed7903d05f72a3484077ae3f8
Jean-Michel <jean-michel.picod@cassidian.com>
2013-02-27 22:55:56 +0100

    [Probes] fix indentation in certificate.py - Thx Juan Manuel for the bug report

--- DPAPI/Probes/certificate.py
+++ DPAPI/Probes/certificate.py
@@ -65,17 +65,17 @@ class PrivateKeyBlob(DPAPIProbe):
             """subclass for ASN.1 sequence representing the RSA key pair.
             Mainly useful to export the key to OpenSSL
             """
-                componentType = namedtype.NamedTypes(
-                        namedtype.NamedType('version', univ.Integer()),
-                        namedtype.NamedType('modulus', univ.Integer()),
-                        namedtype.NamedType('pubexpo', univ.Integer()),
-                        namedtype.NamedType('privexpo', univ.Integer()),
-                        namedtype.NamedType('prime1', univ.Integer()),
-                        namedtype.NamedType('prime2', univ.Integer()),
-                        namedtype.NamedType('exponent1', univ.Integer()),
-                        namedtype.NamedType('exponent2', univ.Integer()),
-                        namedtype.NamedType('coefficient', univ.Integer())
-                        )
+            componentType = namedtype.NamedTypes(
+                    namedtype.NamedType('version', univ.Integer()),
+                    namedtype.NamedType('modulus', univ.Integer()),
+                    namedtype.NamedType('pubexpo', univ.Integer()),
+                    namedtype.NamedType('privexpo', univ.Integer()),
+                    namedtype.NamedType('prime1', univ.Integer()),
+                    namedtype.NamedType('prime2', univ.Integer()),
+                    namedtype.NamedType('exponent1', univ.Integer()),
+                    namedtype.NamedType('exponent2', univ.Integer()),
+                    namedtype.NamedType('coefficient', univ.Integer())
+                    )
 
         def parse(self, data):
             self.magic = data.eat("4s") # RSA2
