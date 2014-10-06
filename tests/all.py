1efb957aaf2942ffcc8863907b38b9d613dd5dc5
Jean-Michel Picod <jean-michel.picod@cassidian.com>
2014-10-06 23:45:59 +0200

    Add a script to run all tests

---
+++ tests/all.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python2
+# -*- encoding: utf-8 -*-
+
+# ############################################################################
+##                                                                         ##
+## This file is part of DPAPIck                                            ##
+## Windows DPAPI decryption & forensic toolkit                             ##
+##                                                                         ##
+##                                                                         ##
+## Copyright (C) 2010, 2011 Cassidian SAS. All rights reserved.            ##
+## This document is the property of Cassidian SAS, it may not be copied or ##
+## circulated without prior licence                                        ##
+##                                                                         ##
+##  Author: Jean-Michel Picod <jmichel.p@gmail.com>                        ##
+##                                                                         ##
+## This program is distributed under GPLv3 licence (see LICENCE.txt)       ##
+##                                                                         ##
+#############################################################################
+
+from blob import *
+from credhist import *
+from crypto import *
+from masterkey import *
+import unittest
+
+if __name__ == '__main__':
+    unittest.main()
